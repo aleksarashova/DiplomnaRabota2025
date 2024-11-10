3532ee0f4ec82d608527acb9b0919ca47cc13842
aleks <aleksa_rashova@abv.bg>
2024-11-10 21:56:40 +0300

    feat: created a navbar for the profile page

---
+++ frontend/src/components/bars/navbars/NavbarForProfile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Link } from 'react-router-dom';
+
+import { FaHome } from "react-icons/fa";
+import { IoMdLogOut } from "react-icons/io";
+
+const NavbarForProfile = () => {
+    return (
+        <nav className="navbar" id="navbarForForms">
+            <ul className="nav-list">
+                <li>
+                    <Link to="/" className="link">
+                        <div className="icon-box-nav"><FaHome className="icon-nav"/></div>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/login" className="link">
+                        <div className="icon-box-nav-last"><IoMdLogOut className="icon-nav"/></div>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavbarForProfile;
