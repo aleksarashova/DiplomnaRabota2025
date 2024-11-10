9b12c7867245d47daab5e7b98d9a8c0a1d54106d
aleks <aleksa_rashova@abv.bg>
2024-11-10 21:50:25 +0300

    feat: created a reset password form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import LoginForm from './components/forms/LoginForm';
 import RegisterForm from './components/forms/RegisterForm';
 import ForgotPasswordForm from "./components/forms/ForgotPasswordForm";
+import ResetPasswordForm from "./components/forms/ResetPasswordForm";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<LoginForm />}></Route>
         <Route path="/register" element={<RegisterForm />}></Route>
         <Route path="/forgot-password" element={<ForgotPasswordForm />}></Route>
+        <Route path="/reset-password" element={<ResetPasswordForm />}></Route>
       </Routes>
     </div>
   );
---
+++ frontend/src/components/forms/ResetPasswordForm.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { FaLock } from "react-icons/fa";
+
+const ResetPasswordForm = () => {
+    return (
+        <div className="form-page">
+            <div className="form-wrapper" id="formWrapperResetPassword">
+                <div className="form-content" id="formContentResetPassword">
+                    <form action="">
+                        <h1 className="form-title" id="formTitleResetPassword">Reset password</h1>
+                        <div className="form-input-box">
+                            <div className="form-icon"><FaLock/></div>
+                            <input type="text" placeholder="New password" className="form-input" required/>
+                        </div>
+                        <div className="form-input-box">
+                            <div className="form-icon"><FaLock/></div>
+                            <input type="text" placeholder="Repeat new password" className="form-input" required/>
+                        </div>
+                        <button type="submit" className="form-button" id="formButtonResetPassword">Reset password
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ResetPasswordForm;
