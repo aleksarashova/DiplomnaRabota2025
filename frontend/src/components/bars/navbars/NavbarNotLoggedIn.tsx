8bbcdbb656d05d03d57cadd2bdc6d7e3b90f42c2
aleks <aleksa_rashova@abv.bg>
2024-11-10 21:59:28 +0300

    feat: created a navbar for not logged in users(guests)

---
+++ frontend/src/components/bars/navbars/NavbarNotLoggedIn.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Link } from 'react-router-dom';
+
+import { FaHome } from "react-icons/fa";
+import { IoMdLogIn } from "react-icons/io";
+import { MdAppRegistration } from "react-icons/md";
+
+import SearchBar from "../searchbar/searchbar";
+
+const NavbarNotLoggedIn = () => {
+    return (
+        <nav className="navbar">
+            <div className="search-bar-nav">
+                <SearchBar />
+            </div>
+            <ul className="nav-list">
+                <li>
+                    <Link to="/" className="link">
+                        <div className="icon-box-nav"><FaHome className="icon-nav"/></div>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/login" className="link">
+                        <div className="icon-box-nav"><IoMdLogIn className="icon-nav"/></div>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/register" className="link">
+                        <div className="icon-box-nav-last"><MdAppRegistration className="icon-nav"/></div>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavbarNotLoggedIn;
