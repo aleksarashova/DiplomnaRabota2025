7db8e0d395701d5685c64bcf16f0e66d1c29ead2
aleks <aleksa_rashova@abv.bg>
2024-11-10 22:00:41 +0300

    feat: created a navbar for the logged in users

---
+++ frontend/src/components/bars/navbars/NavbarLoggedIn.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Link } from 'react-router-dom';
+
+import { FaHome } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { IoMdLogOut } from "react-icons/io";
+
+import SearchBar from "../searchbar/searchbar";
+
+const NavbarLoggedIn = () => {
+    return (
+        <nav className="navbar">
+            <div className="search-bar-nav">
+                <SearchBar/>
+            </div>
+            <ul className="nav-list">
+                <li>
+                    <Link to="/" className="link">
+                        <div className="icon-box-nav"><FaHome className="icon-nav"/></div>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/myProfile" className="link">
+                        <div className="icon-box-nav"><CgProfile className="icon-nav"/></div>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/login" className="link">
+                        <div className="icon-box-nav-last"><IoMdLogOut className="icon-nav"/></div>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavbarLoggedIn;
