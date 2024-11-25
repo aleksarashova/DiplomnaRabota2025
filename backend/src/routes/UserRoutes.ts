30722ce2e9a201b33a1c3c55016f74c1b8114f89
aleks <aleksa_rashova@abv.bg>
2024-11-26 00:09:02 +0300

    feat: created the first core routes for the users and their profiles

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -6,4 +6,9 @@ dotenv.config();
 
 const router: Router = express.Router();
 
+router.post("/register");
+router.post("/login");
+router.post("/update-profile");
+router.post("/delete-profile");
+
 export default router;
