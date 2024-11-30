bbad2b29e4e2f5f02856d662272c3a553b0807a3
aleks <aleksa_rashova@abv.bg>
2024-11-30 20:20:26 +0300

    feat: created the validation for the login credentials in the user middleware

--- backend/src/middlewares/UserMiddleware.ts
+++ backend/src/middlewares/UserMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-import {findUserByEmail, findUserByUsername} from "../services/UserService";
+import {findUserByEmail, findUserByUsername, checkForRightPassword} from "../services/UserService";
 
 export const checkUniquenessRegister = async (
     req: Request,
@@ -23,6 +23,35 @@ export const checkUniquenessRegister = async (
         next();
     } catch (error) {
         console.error("Error during uniqueness check:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const checkCredentialsLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void | any> => {
+    try {
+        const { username, password } = req.body;
+
+        const existingUser = await findUserByUsername(username);
+        if (!existingUser) {
+            return res.status(400).json({ message: "No such user. Please register first." });
+        }
+
+        if(!existingUser.is_verified) {
+            return res.status(400).json({ message: "You should verify your account first." });
+        }
+
+        const isPasswordValid = await checkForRightPassword(password, existingUser.password_hash);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid password. Please try again." });
+        }
+
+        next();
+    } catch (error) {
+        console.error("Error during credentials check:", error);
         return res.status(500).json({ message: "Internal server error" });
     }
 }
--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -3,12 +3,12 @@ import express, { Router } from "express";
 import {login, register} from "../controllers/UserController";
 import {validateRequest} from "../middlewares/RequestValidationMiddleware";
 import {userRegisterSchema} from "../validationSchemas/UserValidation";
-import {checkUniquenessRegister} from "../middlewares/UserMiddleware";
+import {checkCredentialsLogin, checkUniquenessRegister} from "../middlewares/UserMiddleware";
 
 const router: Router = express.Router();
 
 router.post("/register", validateRequest(userRegisterSchema), checkUniquenessRegister, register);
-router.post("/login", login);
+router.post("/login", checkCredentialsLogin, login);
 router.post("/update-profile");
 router.post("/delete-profile");
 
