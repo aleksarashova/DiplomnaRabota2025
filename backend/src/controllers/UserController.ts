3234166cb486ae62b8be0d2b65bcfe2e6e1194a5
aleks <aleksa_rashova@abv.bg>
2024-11-30 20:07:05 +0300

    feat: created the function for login in the user controller with creating a jwt

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
-import {createUser} from "../services/UserService";
+import {createUser, findUserByUsername, updateUserLoggedIn} from "../services/UserService";
 
 export const register = async (req: Request, res: Response): Promise<void | any> => {
     try {
@@ -12,6 +13,45 @@ export const register = async (req: Request, res: Response): Promise<void | any>
         return res.status(201).json({ message: "User registered successfully", user: newUser });
     } catch (error) {
         console.error("Error during registration:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const login = async (req: Request, res: Response): Promise<void | any> => {
+    const { username } = req.body;
+    try {
+        const user = await findUserByUsername(username);
+        if (user) {
+            const accessToken = jwt.sign(
+                {
+                    userId: user._id,
+                    username: user.username,
+                    role: user.role,
+                },
+                process.env.JWT_SECRET!,
+                {
+                    expiresIn: process.env.JWT_EXP,
+                }
+            );
+
+            const refreshToken = jwt.sign(
+                {userId: user._id},
+                process.env.JWT_SECRET!,
+                {
+                    expiresIn: process.env.JWT_REFRESH_EXP,
+                }
+            );
+
+            await updateUserLoggedIn(username);
+
+            res.status(200).json({
+                message: "User logged in successfully.",
+                accessToken,
+                refreshToken,
+            });
+        }
+    } catch (error) {
+        console.error("Error during login:", error);
         return res.status(500).json({ message: "Internal server error" });
     }
 }
--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
 
-import { register } from "../controllers/UserController";
+import {login, register} from "../controllers/UserController";
 import {validateRequest} from "../middlewares/RequestValidationMiddleware";
 import {userRegisterSchema} from "../validationSchemas/UserValidation";
 import {checkUniquenessRegister} from "../middlewares/UserMiddleware";
@@ -8,7 +8,7 @@ import {checkUniquenessRegister} from "../middlewares/UserMiddleware";
 const router: Router = express.Router();
 
 router.post("/register", validateRequest(userRegisterSchema), checkUniquenessRegister, register);
-router.post("/login");
+router.post("/login", login);
 router.post("/update-profile");
 router.post("/delete-profile");
 
